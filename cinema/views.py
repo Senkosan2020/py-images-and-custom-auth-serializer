4d4ca1679f5a00aead552ff944c08715fa529776
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-09-06 02:27:34 +0200

    'Solution'

--- cinema/views.py
+++ cinema/views.py
@@ -64,49 +64,61 @@ class CinemaHallViewSet(
 
 
 class MovieViewSet(
-    ReadOnlyModelViewSet,
+    mixins.ListModelMixin,
     mixins.CreateModelMixin,
-    GenericViewSet,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
 ):
     queryset = Movie.objects.prefetch_related("genres", "actors")
-    serializer_class = MovieSerializer
+    serializer_class = MovieWriteSerializer
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
+    pagination_class = None
 
     @staticmethod
-    def _params_to_ints(qs):
-        """Converts a list of string IDs to a list of integers"""
-        return [int(str_id) for str_id in qs.split(",")]
+    def _params_to_ints(value: str) -> List[int]:
+        return [int(x) for x in value.split(",") if x.strip().isdigit()]
 
-    def get_queryset(self):
-        """Retrieve the movies with filters"""
-        title = self.request.query_params.get("title")
-        genres = self.request.query_params.get("genres")
-        actors = self.request.query_params.get("actors")
+    def get_queryset(self) -> QuerySet:
+        queryset = super().get_queryset()
+        title_param = self.request.query_params.get("title")
+        genres_param = self.request.query_params.get("genres")
+        actors_param = self.request.query_params.get("actors")
 
-        queryset = self.queryset
-
-        if title:
-            queryset = queryset.filter(title__icontains=title)
-
-        if genres:
-            genres_ids = self._params_to_ints(genres)
-            queryset = queryset.filter(genres__id__in=genres_ids)
-
-        if actors:
-            actors_ids = self._params_to_ints(actors)
-            queryset = queryset.filter(actors__id__in=actors_ids)
+        if title_param:
+            queryset = queryset.filter(title__icontains=title_param)
+        if genres_param:
+            ids = self._params_to_ints(genres_param)
+            if ids:
+                queryset = queryset.filter(genres__id__in=ids)
+        if actors_param:
+            ids = self._params_to_ints(actors_param)
+            if ids:
+                queryset = queryset.filter(actors__id__in=ids)
 
         return queryset.distinct()
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> Type[Serializer]:
         if self.action == "list":
             return MovieListSerializer
-
         if self.action == "retrieve":
             return MovieDetailSerializer
+        if self.action == "upload_image":
+            return MovieImageSerializer
+        return MovieWriteSerializer
 
-        return MovieSerializer
+    @action(
+        methods=["post"],
+        detail=True,
+        url_path="upload-image",
+        parser_classes=[MultiPartParser, FormParser],
+    )
+    def upload_image(self, request, pk=None):
+        movie = self.get_object()
+        serializer = self.get_serializer(movie, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=200)
 
 
 class MovieSessionViewSet(viewsets.ModelViewSet):
@@ -178,60 +190,3 @@ class OrderViewSet(
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-
-class MovieViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    viewsets.GenericViewSet
-):
-    queryset = Movie.objects.all()
-    serializer_class = MovieWriteSerializer
-    permission_classes = [IsAdminOrIfAuthenticatedReadOnly]
-    pagination_class = None
-
-    @staticmethod
-    def _params_to_ints(value: str) -> List[int]:
-        return [int(x) for x in value.split(",") if x.strip().isdigit()]
-
-    def get_queryset(self) -> QuerySet:
-        queryset = super().get_queryset()
-        title_param = self.request.query_params.get("title")
-        genres_param = self.request.query_params.get("genres")
-        actors_param = self.request.query_params.get("actors")
-
-        if title_param:
-            queryset = queryset.filter(title__icontains=title_param)
-        if genres_param:
-            ids = self._params_to_ints(genres_param)
-            if ids:
-                queryset = queryset.filter(genres__id__in=ids)
-        if actors_param:
-            ids = self._params_to_ints(actors_param)
-            if ids:
-                queryset = queryset.filter(actors__id__in=ids)
-
-        return queryset.distinct()
-
-    def get_serializer_class(self) -> Type[Serializer]:
-        if self.action == "list":
-            return MovieListSerializer
-        if self.action == "retrieve":
-            return MovieDetailSerializer
-        if self.action == "upload_image":
-            return MovieImageSerializer
-        return MovieWriteSerializer
-
-    @action(
-        methods=["post"],
-        detail=True,
-        url_path="upload-image",
-        parser_classes=[MultiPartParser, FormParser],
-    )
-    def upload_image(self, request, pk=None):
-        movie = self.get_object()
-        serializer = MovieImageSerializer(movie, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=200)
